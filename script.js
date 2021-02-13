bcefce85d403eb5709ce5fd4e37c94ed6d943ca5
Jorge Perez <jorgealbertopp@hotmail.com>
2021-02-13 15:29:46 -0600

    Added a card manually with DOM Manipulation

--- script.js
+++ script.js
@@ -16,9 +16,22 @@ let theHobbit = new Books("The Hobbit", "J.R.R Tolkien", 295, "not read yet");
 let theLOTR = new Books("The Lord Of The Rings", "J.R.R Tolkien", 350, "read");
 let GOT = new Books("Game of Thrones", "G.R.R Martin", 134, "read");
 let sombra = new Books("La Sombra del Viento", "Carlos Ruiz Zafon", 320, "read");
-let sophia = new Books("Sophie's World", "Jostein Gaarder", 500, "read");
+let sophia = new Books("Sophie's World", "Jostein Gaarder", 500, "Yes");
 
-myLibrary.push(theHobbit, theLOTR, GOT, sombra, sophia);
-console.table(myLibrary);
+function addBooktoLibrary(){
+    myLibrary.push(sophia);
+}
 
+addBooktoLibrary();
+console.log(myLibrary[0].title);
+console.log(myLibrary[0].author);
+console.log(myLibrary[0].numPages);
+console.log(myLibrary[0].haveRead);
 
+//GENERATING ONE CARD MANUALLY
+
+let generator = document.getElementById("cards-container");
+let creator = document.createElement("div");
+creator.id = "card1";
+creator.innerHTML = `<span>Title:</span><p>${myLibrary[0].title}</p><span>Author:</span><p>${myLibrary[0].author}</p><span>Volumes:</span><p>${myLibrary[0].numPages}</p><span>Have you read it?:</span><p>${myLibrary[0].haveRead}</p>`;
+generator.appendChild(creator);
