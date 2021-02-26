2c1c86956bd94c8de36696567dc0d8088606b662
Jorge Perez <jorgealbertopp@hotmail.com>
2021-02-25 21:49:45 -0600

    Centering the pop-up

--- script.js
+++ script.js
@@ -41,6 +41,7 @@ generator.appendChild(creator);
 
 function openForm() {
     document.getElementById("popupForm").style.display = "block";
+    
   }
   function closeForm() {
     document.getElementById("popupForm").style.display = "none";
